4b137f5901040271c8a4d6b394d30b3625033875
zgaroutt <zachary.garoutte@wsu.edu>
2025-03-18 14:14:41 -0700

    Fixed issues stopping backend from working

--- LivingAtlas1-main/backend/database.py
+++ LivingAtlas1-main/backend/database.py
@@ -1,24 +1,23 @@
 
 
-
-
-
-# database.py
-
 import psycopg2
 from psycopg2 import OperationalError, errorcodes, errors
 
-try:
-    # Old ElephantSQL database
-    # conn = psycopg2.connect('postgres://tgpxaiud:5MBj7NqaMmQuFAS6iVHk8dmThMl3oc1M@bubble.db.elephantsql.com/tgpxaiud')
+conn = None  # Ensure conn is always defined
 
-    # Aiven database
+try:
+    # Aiven database connection
     conn = psycopg2.connect('postgres://avnadmin:AVNS_vBiPLJt2YvOvpq0V7Ha@pg-1b73eb6f-livingatlas-livingatlasdb.l.aivencloud.com:13918/defaultdb?sslmode=require')
     print('Connection Success!')
     connectionsucceeded = True
 
-except:
-   print("Unable to connect to the database")
+except OperationalError as e:
+    print("Unable to connect to the database")
+    print(f"Error: {e}")
+    connectionsucceeded = False  # Mark connection failure
 
-# Open a cursor to execute SQL queries
-cur = conn.cursor()
+# Ensure cursor is only created if connection succeeded
+if conn:
+    cur = conn.cursor()
+else:
+    cur = None
